1dca321381ccc2d0de53a8cbe1aa871284132066
xwuun <soyun7305@naver.com>
2024-12-05 21:04:37 +0900

    삭제

--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//한글 주석 테스트 
-int main(int argc, char *argv[]) {
-	return 0;
-}
